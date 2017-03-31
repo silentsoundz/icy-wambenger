0f1c517c126e45973416cdabc1c6fa0975c9c465
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-03-30 17:54:23 -0700

    added and tests 7 new functions

---
+++ src/maxArray.js
@@ -0,0 +1,6 @@
+const madMax = (array) => {
+  let tipTop = (array)
+tipTop.sort(function(a, b){return b-a})
+return tipTop[0]
+}
+module.exports = madMax
---
+++ src/upperFirst.js
@@ -0,0 +1,5 @@
+const upperFirst = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+module.exports = upperFirst
